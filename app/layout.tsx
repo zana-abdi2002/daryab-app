ec6043e2807d55f9ffe6e57a0ec230f2e24b2bc9
Zana-Abdi <zana.abdi@hotmail.com>
2025-04-19 12:47:10 +0330

    feat(app/layout.tsx) add background color to all pages and chang language and direction to farsi - rtl
    add bg color to all pages by changing bg of app/layout.tsx
    change lang and dir in app/layout.tsx, <html> tag
    
    update @tailwindcss/postcss package
    
    add styles.md to .gitignore so you can keep your color palatte on your device
    
    TESTING: run "npm dev run" and you should be able to see bg color on
    home (url/) and meeting (url/meeting/12354).
    now all text is rtl (press ctrl + a to see the texts )

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fa-IR" dir="rtl">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#161925]`}
       >
         {children}
       </body>
