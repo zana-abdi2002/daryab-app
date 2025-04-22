b07ad2669a81d016ec80dfe97ae1b697d04b7784
Zana-Abdi <zana.abdi@hotmail.com>
2025-04-22 14:48:16 +0330

    feat(NavBar): add a Navbar
    add a navbar containing clickable app logo
    
    optional to add <MobileNav /> for further developement
    
    TESTING: run "npm run dev" and click on app logo on navbar
    BREAKING CHANGE: the app has a navbar now

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
+import MobileNav from "./MobileNav";
 
-function Navbar() {
-  return <div>Navbar</div>;
-}
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between fixed z-50 w-full bg-[#1C1F2E] px-4 py-3 lg:px-10">
+      {/* LOGO ------------------------------------------------ */}
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="Daryab logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          دریاب
+        </p>
+      </Link>
+      {/* ------------------------------------------------------- */}
+
+      <div className="justify-between gap-5">
+        {/* User management */}
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
