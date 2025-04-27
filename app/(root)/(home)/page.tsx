26b3796baefdfdfe27781462dac43737a4333025
Zana-Abdi <zana.abdi@hotmail.com>
2025-04-27 15:55:57 +0330

    update, style(packages, signUp card) update packages and changed some styles
    update package.json and package-lock.json using "npm update"

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,8 +3,14 @@ import React from "react";
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString();
-  const date = "";
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+  const date = new Intl.DateTimeFormat("fa-IR", { dateStyle: "full" }).format(
+    now
+  );
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
@@ -12,13 +18,17 @@ const Home = () => {
         className="h-[300px] w-full rounded-[20px] bg-cover bg-no-repeat"
         style={{ backgroundImage: "url('/images/hero-background.png')" }}
       >
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
-            جلسه پیش رو در: {time}
+        <div className="flex h-full flex-col justify-between max-md:px-2 max-md:py-4 lg:p-5">
+          <h2 className="glassmorphism max-w-fit rounded-[2px] px-2 py-1 mr-9 mt-2 text-center text-base font-normal">
+            جلسه پیش رو در: 22:10
           </h2>
-          <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">11:30 </h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          <div className="flex flex-col gap-2 pr-4 pb-2 ">
+            <h1 className="text-4xl font-extrabold lg:text-7xl text-shadow-lg">
+              {time}
+            </h1>
+            <p className="text-lg font-medium text-sky-1 text-shadow-lg lg:text-2xl">
+              {date.split(" ").reverse().join(" ").replace(",", "")}
+            </p>
           </div>
         </div>
       </div>
